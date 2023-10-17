004aa2122dcd1d69372d75c05a4292688e603705
kmai2s <khai.mai@smail.inf.h-brs.de>
2023-10-17 23:09:05 +0200

    commit

--- test/org/hbrs/se1/ws23/uebung1/test/GermanTranslatorTest.java
+++ test/org/hbrs/se1/ws23/uebung1/test/GermanTranslatorTest.java
@@ -1,17 +1,43 @@
 package org.hbrs.se1.ws23.uebung1.test;
 
 import org.hbrs.se1.ws23.uebung1.view.Client;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GermanTranslatorTest {
-
+    Client c;
+    int number;
+    @BeforeEach
+    void init() {
+        c =  new Client();
+    }
     @Test
-    void aPositiveTest() {
-        Client c = new Client();
-        int number = 0;
+    void zeroTest() {
+        number = 0;
         String value = c.display(number);
         assertEquals(value, "Übersetzung der Zahl 0 nicht möglich (v1.9)");
+    }
+
+    @Test
+    void biggerThan10Test() {
+        number = 11;
+        String value = c.display(number);
+        assertEquals(value, "Übersetzung der Zahl 11 nicht möglich (v1.9)");
+    }
+
+    @Test
+    void positiveTest() {
+        number = 3;
+        String value = c.display(number);
+        assertEquals(value, "drei");
+    }
+
+    @Test
+    void negativeTest() {
+        number = -5;
+        String value = c.display(number);
+        assertEquals(value, "Übersetzung der Zahl -5 nicht möglich (v1.9)");
     }
 }
